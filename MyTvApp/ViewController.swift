5073a4f89817ece6f59869d905e73aa3b9f3e009
kevin <kevin.hijlkema@meteoconsult.com>
2020-01-08 15:26:13 +0100

    Initial Commit

---
+++ MyTvApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTvApp
+//
+//  Created by kevin hijlkema on 08/01/2020.
+//  Copyright © 2020 KhiProjects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
